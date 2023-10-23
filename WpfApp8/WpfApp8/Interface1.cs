735c5783f04afaf56f5f2601b2807586f5dd9788
JPKakaAG <Gjopka@yandex.ru>
2023-10-23 12:39:44 +0300

    Практическая 8

---
+++ WpfApp8/WpfApp8/Interface1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp8
+{
+    public interface IPerson
+    {
+        string Name { get; } // Свойство "Имя"
+        void Print(); // Метод "Печать"
+    }
+
+    // Реализуем класс "Отец"
+    public class Father : IPerson
+    {
+        public string Name { get; private set; } // Свойство "Имя"
+
+        public Father(string name)
+        {
+            Name = name;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Имя: " + Name);
+        }
+    }
+
+    // Реализуем класс "Ребенок"
+    public class Child : IPerson, IComparable<Child>, ICloneable
+    {
+        public string Name { get; private set; } // Свойство "Имя"
+        public string Patronymic { get; private set; } //Свойство "Отчество"
+        public Father Father { get; private set; } // Свойство "Отец"
+
+        public Child(string name, string patronymic, Father father)
+        {
+            Name = name;
+            Patronymic = patronymic;
+            Father = father;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Имя: " + Name + ", Отчество: " + Patronymic);
+        }
+
+        // Реализуем метод сравнения по фамилии
+        public int CompareTo(Child other)
+        {
+            if (other == null)
+                return 1;
+
+            string[] lastName1 = Name.Split(' ');
+            string[] lastName2 = other.Name.Split(' ');
+
+            return string.Compare(lastName1[0], lastName2[0], StringComparison.Ordinal);
+        }
+
+        // Реализуем метод клонирования
+        public object Clone()
+        {
+            return new Child(Name, Patronymic, Father);
+        }
+    }
+
+}
---
+++ WpfApp8/WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил лучший разработчик: Девяткин Вадим Евгеньевич\r\nПрактическая №8\r\nСоздать интерфейс - человек, у которого есть имя, функция печати. \r\nСоздать класс" + 
+                    "отец, у которого функция печати выводит имя.\r\nСоздать класс ребенок, у которого" +
+                    "есть отец, отчество, функция печати выводит имя и отчество.Классы должны" +
+                    "включать конструкторы.Сравнение производить по фамилии");
+        }
+
+        private void btnRes_Click(object sender, RoutedEventArgs e)
+        {
+            // Создаем объекты
+            Father father = new Father("Иван");
+            Child child1 = new Child("Петров Иван", "Иванович", father);
+            Child child2 = new Child("Сидоров Петр", "Алексеевич", father);
+
+            // Вызываем методы объектов
+            child1.Print();
+            child2.Print();
+
+            // Выполняем сравнение объектов
+            int result = child1.CompareTo(child2);
+            string comparisonResult = result == 0 ? "равны" : result < 0 ? "меньше" : "больше";
+            tbRes.Text = ("Сравнение: " + child1.Name + " и " + child2.Name + " - " + comparisonResult);
+
+            // Выполняем клонирование объекта
+            Child childClone = (Child)child1.Clone();
+            childClone.Print();
+        }
+    }
+}
